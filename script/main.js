ad071d893a890970c75b489339d0c04e2ce90c58
natashaadler <n_adler@fanshaweonline.ca>
2021-11-27 02:03:35 -0500

    Added basic portfolio structure, js

--- script/main.js
+++ script/main.js
@@ -1,50 +1,50 @@
 //import files go on top
 
 
-//add reg code here    **FAV THINGS CODE
+//add reg code here    **FAV THINGS CODE changed to suit hero of portfolio   
 
 (() => {
-    const   favThingsButtons = document.querySelector("#favThingsButtons"),
-            theFavButtonTemplate = document.querySelector("#favThingsButtonTemplate").content;
+    const   portfolioButton = document.querySelector("#portfolioButton"),
+            portfolioButtonTemplate = document.querySelector("#portfolioButtonTemplate").content;
 
-    let favourteItems = [];
+    let portfolioItems = [];
 
     // The FETCH Function and get my Fav Things DATA
-    function getFavData() {
+    function getPortfolioData() {
         fetch("./data.json")
             .then(res => res.json())
             .then(data => {
                 // save to get it later
-                favourteItems = data;
+                portfolioItems = data;
 
-                buildFavThings(data);
+                buildPortfolioItems(data);
             })
         .catch(error => {
             console.error(error);
         });
     }
 
-    function buildFavThings(info) {
+    function buildPortfolioItems(info) {
 
-        const favouriteNames = Object.keys(info);
+        const portfolioNames = Object.keys(info);
 
-        updateText(info[favouriteNames[0]])
+        updateText(info[portfolioNames[0]])
 
         // this adds all the thumbnails at the bottom of the screen
-        favouriteNames.forEach(favorite => {
-            let panel = theFavButtonTemplate.cloneNode(true);
+        portfolioNames.forEach(portfolio => {
+            let panel = portfolioButtonTemplate.cloneNode(true);
             let section = panel.children;
 
-            section[0].dataset.name = favorite
+            section[0].dataset.name = portfolio
 
             // adds an image
-            section[0].querySelector("img").src = `images/${info[favorite].favpic}`;
+            section[0].querySelector("img").src = `images/${info[portfolio].portfolioImage}`;
 
-            favThingsButtons.appendChild(panel);
+            portfolioButton.appendChild(panel);
         });
 
         // Goes through each button and adds a 'click' event
-        let buttons = document.querySelectorAll(".favourite_button");
+        let buttons = document.querySelectorAll(".portfolio_button");
         buttons.forEach(button => {
             button.addEventListener('click', clickFavourite)
         });
@@ -52,26 +52,25 @@
 
     // this 'click' function gets the name and then changes
     function clickFavourite(event) {
-        const favorite = event.currentTarget.dataset.name;
-        updateText(favourteItems[favorite])
+        const portfolio = event.currentTarget.dataset.name;
+        updateText(portfolioItems[portfolio])
     }
 
     // this function adds the content 
     function updateText(data) {
-        const landingBox = document.querySelector("#favThings");
+        const landingBox = document.querySelector("#portfolio_section");
         let section = landingBox.children;
         // this adds the image
         // first div which are the images 
-        section[1].querySelector("img").src = `images/${data.favpic}`;
+        section[1].querySelector("img").src = `images/${data.portfolioImage}`;
 
         // this gets the second div children which is the text
         let text = section[0].children
         // this updates the text
         text[0].textContent = data.name;
-        text[1].textContent = data.heading;
-        text[2].textContent = data.description;
+        text[1].textContent = data.sub_heading;
     }
 
-    getFavData();
+    getPortfolioData();
 })();
     
